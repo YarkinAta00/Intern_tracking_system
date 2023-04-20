6b6278798a31b9c99cdbf5e284fe1b2d8e2983ae
yarkinata <yarkinata6268@gmail.com>
2023-04-20 12:29:38 +0300

    chart config added

---
+++ deneme/src/chartConfig.js
@@ -0,0 +1,15 @@
+export const data = {
+    labels: ['Remaining Days', 'Past Days'],
+    datasets: [
+      {
+        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+        data: [15, 25]
+      }
+    ]
+  }
+  
+  export const options = {
+    responsive: true,
+    maintainAspectRatio: false
+  }
+  
