5a4527c49326fb7c080c38a352aa03c7bb2ff27f
yarkinata <yarkinata6268@gmail.com>
2023-04-20 12:29:15 +0300

    Dougnut chart is added to Intern Profile

--- deneme/src/InternProfile.vue
+++ deneme/src/InternProfile.vue
@@ -2,63 +2,35 @@
 import Button from './components/Button.vue'
 import PageTitle from './components/PageTitle.vue';
 import ProfilePhoto from './components/ProfilePhoto.vue';
-import Footer from './Footer.vue'
+import Footer from './Footer.vue';
+
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
 export default {
-
+    //CV görebilceğimiz ekran popup olabilir
     data() {
-        return {
-            interns: [{
-                id: 1,
-                name: "Support Engineer",
-                applicationCount: "2",
-                department: "Support Team",
-
-            },
-            {
-                id: 2,
-                name: "Full Stack Developer",
-                applicationCount: "5",
-                department: "Developer Team",
-
-            },
-            {
-                id: 3,
-                name: "Business Analyst",
-                applicationCount: "3",
-                department: "Project Management Team",
-
-            },
-            {
-                id: 4,
-                name: "Test Autiomation Developer",
-                applicationCount: "2",
-                department: "Testing Team",
-
-            },
-            {
-                id: 5,
-                name: "Front-end Developer",
-                applicationCount: "5",
-                department: "Development Team",
-
-            },
-            ],
-        }
+        return chartConfig
     },
     components: {
         Footer,
         Button,
         PageTitle,
-        ProfilePhoto
+        ProfilePhoto,
+        Doughnut,
     }
 }
 </script>
 
 <template>
-    <div class="container d-flex align-items-center justify-content-center my-4">
+    <div class="container d-flex align-items-center justify-content-center mt-4">
         <div class="row">
             <div class="col">
-                <div class="card text-bg-light" style="width: 18rem;">
+                <div class="card text-bg-light mb-4" style="width: 18rem;">
                     <ProfilePhoto class="card-img-top" />
                     <div class="card-body">
                         <h5 class="card-title">Name Surname</h5>
@@ -68,12 +40,21 @@ export default {
                         <li class="list-group-item">Position</li>
                         <li class="list-group-item">Department</li>
                         <li class="list-group-item">Lead of Department</li>
-                        <li class="list-group-item">Position</li>
-                        <li class="list-group-item">Position</li>
+                        <li class="list-group-item">
+                            <button type="button" class="btn btn-link text-decoration-none" data-bs-toggle="modal"
+                                data-bs-target="#exampleModal">Show CV</button>
+                        </li>
                     </ul>
                     <div class="card-body">
-                        <a href="#" class="card-link">Maybe Github link</a>
-                        <a href="#" class="card-link">Maybe Linkedln link</a>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <a href="#" class="card-link text-decoration-none">Maybe Github link</a>
+                            </li>
+
+                            <li class="list-group-item">
+                                <a href="#" class="card-link text-decoration-none">Maybe Linkedln link</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -88,9 +69,7 @@ export default {
                         </ul>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 30rem;">
+                <div class="card mt-4" style="width: 30rem;">
                     <h5 class="card-header">Contact Information</h5>
                     <div class="card-body">
                         <ul class="list-group list-group-flush">
@@ -101,9 +80,32 @@ export default {
                     </div>
                 </div>
             </div>
+            <div class="mx-5 col">
+                <div>
+                    <Doughnut :data="data" :options="options"/>
+                </div>
+            </div>
         </div>
-        <div class="row">
-            
+       
+
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">CV</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        CV will be shown here
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
