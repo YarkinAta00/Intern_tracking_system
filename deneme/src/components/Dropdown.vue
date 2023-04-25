9740ceb67f16a18e16799274788bdae5f8fbcc28
yarkinata <yarkinata6268@gmail.com>
2023-04-25 17:16:19 +0300

    Dropdown component added

---
+++ deneme/src/components/Dropdown.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    props:{
+        degrees:{
+        type: String,
+        default:"",
+    },
+    }
+}
+</script>
+<template>
+ <select class="form-select mt-4" aria-label="Default select example">
+      <option selected>Degree</option>
+      <option value="1">Software Engineering</option>
+      <option value="2">Computer Engineering</option>
+      <option value="3">Electric and Electronics Engineering</option>
+    </select>
+</template>
