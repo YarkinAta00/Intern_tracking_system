dcdc6b3fe33a06d87c9d1f94f030a4bc113588ab
yarkinata <yarkinata6268@gmail.com>
2023-04-25 17:15:42 +0300

    Intern profile modified

--- deneme/src/InternProfile.vue
+++ deneme/src/InternProfile.vue
@@ -3,31 +3,23 @@ import Button from './components/Button.vue'
 import PageTitle from './components/PageTitle.vue';
 import ProfilePhoto from './components/ProfilePhoto.vue';
 import Footer from './Footer.vue';
-
-
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Doughnut } from 'vue-chartjs'
-import * as chartConfig from './chartConfig.js'
-
-ChartJS.register(ArcElement, Tooltip, Legend)
-
+import PopupModel from './components/PopupModel.vue';
 export default {
     //CV görebilceğimiz ekran popup olabilir
-    data() {
-        return chartConfig
-    },
+   
     components: {
         Footer,
         Button,
         PageTitle,
         ProfilePhoto,
-        Doughnut,
+        PopupModel,
+       
     }
 }
 </script>
 
 <template>
-    <div class="container d-flex align-items-center justify-content-center mt-4">
+    <div class="container d-flex align-items-center justify-content-center mt-5">
         <div class="row">
             <div class="col">
                 <div class="card text-bg-light mb-4" style="width: 18rem;">
@@ -82,31 +74,11 @@ export default {
             </div>
             <div class="mx-5 col">
                 <div>
-                    <Doughnut :data="data" :options="options"/>
+                   
                 </div>
             </div>
         </div>
-       
-
-
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">CV</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        CV will be shown here
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <PopupModel modelTitle="CV" modelBodyText=" CV will be shown here"/>
     </div>
 </template>
 
