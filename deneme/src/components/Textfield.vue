550b8d4a597c7b365c2eb00f6ef0f218a303c4bf
yarkinata <yarkinata6268@gmail.com>
2023-04-25 17:15:15 +0300

    Login problem fixed

--- deneme/src/components/Textfield.vue
+++ deneme/src/components/Textfield.vue
@@ -1,18 +1,20 @@
 <script>
-export default{
-props:{
-    label:{
-        type: String,
-        default:"",
+export default {
+    props: {
+        name: "Textfield",
+        label: {
+            type: String,
+            default: "",
+        },
+        modelValue: String
     },
-},
+
+    emits: ['update:modelValue'],
 }
 </script>
 
 <template>
     <label>{{ label }}</label>
-  <input
-   v-bind="$attrs" 
-   :placeholder="label"
-   >
+    <input :name="name" :value="modelValue" v-bind="$attrs" :placeholder="label"
+        @input="$emit('update:modelValue', $event.target.value)">
 </template>
