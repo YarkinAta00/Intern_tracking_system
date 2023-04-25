f00d1b59feed7c160cb7e071b536f93ef91c0970
yarkinata <yarkinata6268@gmail.com>
2023-04-25 17:18:20 +0300

    Popup model used in ApplyJobs page

--- deneme/src/ApplyJobs.vue
+++ deneme/src/ApplyJobs.vue
@@ -1,24 +1,29 @@
 <script>
 import PageTitle from './components/PageTitle.vue';
 import Button from './components/Button.vue';
+import PopupModel from './components/PopupModel.vue';
 export default {
     components: {
         PageTitle,
-        Button
+        Button,
+        PopupModel,
     },
     methods: {
-    goAddPosition(){
-                this.$router.push('addposition') 
-            }
-  },
+        goAddPosition() {
+            this.$router.push('addposition')
+        },
+        goApplicationForm() {
+            this.$router.push('applicationform')
+        }
+    },
 }
 </script>
 <template>
-    <div class="row md-10 mx-5"> 
+    <div class="row md-10 mx-5">
         <PageTitle pageTitleValue="Open Positions" />
         <div class="col">
-            <Button @click="goAddPosition" class="px-4 pt-2 mx-5 mt-5" buttonText="Add"/>
-        </div> 
+            <Button @click="goAddPosition" class="px-4 pt-2 mx-5 mt-5" buttonText="Add" />
+        </div>
     </div>
     <div class="col-md-10 mx-auto mt-5">
         <div class="accordion" id="accordionExample">
@@ -26,8 +31,8 @@ export default {
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                     <h5> Support Engineer Intern</h5>
-                       
+                        <h5> Support Engineer Intern</h5>
+
                     </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -51,7 +56,7 @@ export default {
                                 <p>Description</p>
                             </li>
                         </ul>
-                        <Button class="mt-3 px-3 pt-2" buttonText="Apply!"/>
+                        <Button @click="goApplicationForm" class="mt-3 px-3 pt-2" buttonText="Apply!"  />
                     </div>
                 </div>
             </div>
@@ -59,7 +64,7 @@ export default {
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      <h5> Test Autiomation Intern</h5>
+                        <h5> Test Autiomation Intern</h5>
                     </button>
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -83,109 +88,109 @@ export default {
                                 <p>Description</p>
                             </li>
                         </ul>
-                        <Button class="mt-3 px-3 pt-2" buttonText="Apply!"/>
+                        <Button @click="goApplicationForm" class="mt-3 px-3 pt-2" buttonText="Apply!"  />
                     </div>
                 </div>
             </div>
             <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                           <h5> Full Stack Developer Intern </h5>
-                            
-                        </button>
-                    </h2>
-                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <ul>
-                                <li>
-                                    <strong>Department: </strong>
-                                    <br>
-                                    <p>Developer Team</p>
-                                </li>
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <h5> Full Stack Developer Intern </h5>
 
-                                <li>
-                                    <strong>Requirements: </strong>
-                                    <br>
-                                    <p>Requirements</p>
-                                </li>
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <ul>
+                            <li>
+                                <strong>Department: </strong>
+                                <br>
+                                <p>Developer Team</p>
+                            </li>
 
-                                <li>
-                                    <strong>Job Description: </strong>
-                                    <br>
-                                    <p>Description</p>
-                                </li>
-                            </ul>
-                            <Button class="mt-3 px-3 pt-2" buttonText="Apply!"/>
-                        </div>
+                            <li>
+                                <strong>Requirements: </strong>
+                                <br>
+                                <p>Requirements</p>
+                            </li>
+
+                            <li>
+                                <strong>Job Description: </strong>
+                                <br>
+                                <p>Description</p>
+                            </li>
+                        </ul>
+                        <Button @click="goApplicationForm" class="mt-3 px-3 pt-2" buttonText="Apply!"  />
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                           <h5> Business Analyst Intern </h5>
-                           
-                        </button>
-                    </h2>
-                    <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <ul>
-                                <li>
-                                    <strong>Department: </strong>
-                                    <br>
-                                    <p>Project Management Team</p>
-                                </li>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <h5> Business Analyst Intern </h5>
 
-                                <li>
-                                    <strong>Requirements: </strong>
-                                    <br>
-                                    <p>Requirements</p>
-                                </li>
+                    </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <ul>
+                            <li>
+                                <strong>Department: </strong>
+                                <br>
+                                <p>Project Management Team</p>
+                            </li>
 
-                                <li>
-                                    <strong>Job Description: </strong>
-                                    <br>
-                                    <p>Description</p>
-                                </li>
-                            </ul>
-                            <Button class="mt-3 px-3 pt-2" buttonText="Apply!"/>
-                        </div>
+                            <li>
+                                <strong>Requirements: </strong>
+                                <br>
+                                <p>Requirements</p>
+                            </li>
+
+                            <li>
+                                <strong>Job Description: </strong>
+                                <br>
+                                <p>Description</p>
+                            </li>
+                        </ul>
+                        <Button @click="goApplicationForm" class="mt-3 px-3 pt-2" buttonText="Apply!"  />
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                           <h5> Front-end Developer Intern</h5>
-                          
-                        </button>
-                    </h2>
-                    <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <ul>
-                                <li>
-                                    <strong>Department: </strong>
-                                    <br>
-                                    <p>Developer Team</p>
-                                </li>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                        <h5> Front-end Developer Intern</h5>
 
-                                <li>
-                                    <strong>Requirements: </strong>
-                                    <br>
-                                    <p>Medior</p>
-                                </li>
+                    </button>
+                </h2>
+                <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <ul>
+                            <li>
+                                <strong>Department: </strong>
+                                <br>
+                                <p>Developer Team</p>
+                            </li>
 
-                                <li>
-                                    <strong>Job Description: </strong>
-                                    <br>
-                                    <p>Description</p>
-                                </li>
-                            </ul>
-                            <Button class="mt-3 px-3 pt-2" buttonText="Apply!"/>
-                        </div>
+                            <li>
+                                <strong>Requirements: </strong>
+                                <br>
+                                <p>Medior</p>
+                            </li>
+
+                            <li>
+                                <strong>Job Description: </strong>
+                                <br>
+                                <p>Description</p>
+                            </li>
+                        </ul>
+                        <Button @click="goApplicationForm" class="mt-3 px-3 pt-2" buttonText="Apply!"  />
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
