4f25e14f566f473561cef82f5d40bb4a40a15657
yarkinata <yarkinata6268@gmail.com>
2023-04-25 17:16:35 +0300

    chart modified

--- deneme/src/chartConfig.js
+++ deneme/src/chartConfig.js
@@ -1,15 +1,14 @@
 export const data = {
-    labels: ['Remaining Days', 'Past Days'],
-    datasets: [
-      {
-        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-        data: [15, 25]
-      }
-    ]
-  }
-  
-  export const options = {
-    responsive: true,
-    maintainAspectRatio: false
-  }
-  
+  labels: ['Remaining Days', 'Past Days'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [15, 25]
+    }
+  ]
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+}
