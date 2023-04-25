95ee2cf7e9b59e8bfb1d6130f556c4cc89cc7ecf
yarkinata <yarkinata6268@gmail.com>
2023-04-25 17:17:53 +0300

    Home page UI modified

--- deneme/src/HomePage.vue
+++ deneme/src/HomePage.vue
@@ -1,76 +1,49 @@
+
 <script>
-import Button from './components/Button.vue'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
 import PageTitle from './components/PageTitle.vue';
-import Footer from './Footer.vue'
+ChartJS.register(ArcElement, Tooltip, Legend)
+
 export default {
-
-    data() {
-        return {
-            interns: [{
-                id: 1,
-                name: "Yarkın",
-                surname: "Ata",
-                department: "Development Team",
-
-            },
-            {
-                id: 2,
-                name: "Merve",
-                surname: "Cengiz",
-                department: "Support Engineer Team",
-
-            },
-            {
-                id: 3,
-                name: "Hakan",
-                surname: "Atik",
-                department: "Project Management Team",
-
-            },
-            {
-                id: 4,
-                name: "Altay",
-                surname: "Taneri",
-                department: "Testng Team",
-
-            },],
-        }
-    },
-    components: {
-    Footer,
-    Button,
-    PageTitle
-}
-}
+  components: {
+    Doughnut,
+    PageTitle,
+  },
+  data() {
+    return chartConfig
+  },
+};
 </script>
-
 <template>
-    <PageTitle pageTitleValue="Home"/>
-    <div class="container d-flex align-items-center  justify-content-center my-5">
-        <div id="main-container" class="col d-flex align-items-center  justify-content-center">
-            <table class=" table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col" class="fs-5">#</th>
-                        <th scope="col" class="fs-5">Name</th>
-                        <th scope="col" class="fs-5">Surname</th>
-                        <th scope="col" class="fs-5">Department</th>
-                        <th scope="col" class="fs-5">Details</th>
-                    </tr>
-                </thead>
-                <tbody class="table-group-divider">
-                    <tr v-for="(intern, index) in interns">
-                        <td class="fs-5" v-text="intern.id"></td>
-                        <td class="fs-5" v-text="intern.name"></td>
-                        <td class="fs-5" v-text="intern.surname"></td>
-                        <td class="fs-5" v-text="intern.department"></td>
-                        <td>
-                            <Button buttonText="Detail"/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div>
+    <div class="row mt-4">
+      <PageTitle pageTitleValue="Welcome YARKIN ATA" />
     </div>
+
+    <div class="row mt-5">
+      <div class="col-5 ">
+        <Doughnut :data="data" :options="options" id="internChart" />
+
+      </div>
+
+      <div class="col-5 mx-5">
+        <div class="card mb-3">
+          <div class="card-header">
+            <h5 class="card-title">Task Title</h5>
+          </div>
+
+          <div class="card-body">
+            <p class="card-text">Tasdk details will be shown here...</p>
+            <div class="d-flex justify-content-between pt-4">
+              <a href="#" class="  btn btn-warning px-4">Pass</a>
+              <a href="#" class="  btn btn-success px-3">Completed</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<style></style>
