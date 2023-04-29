615486139de4e1c227dea1e38f66933c050f2bcb
yarkinata <yarkinata6268@gmail.com>
2023-04-30 00:39:37 +0300

    Task page added

---
+++ deneme/src/Tasks.vue
@@ -0,0 +1,15 @@
+<script>
+import PageTitle from './components/PageTitle.vue';
+export default{
+components: {
+    PageTitle,
+   
+  },
+}
+</script>
+
+<template>
+    <div>
+        <PageTitle pageTitleValue="Tasks" />
+    </div>
+</template>
