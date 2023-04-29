1e7c0de9d63d96963436a197d4bfe010e9d631b4
yarkinata <yarkinata6268@gmail.com>
2023-04-30 00:42:01 +0300

    hoverset is added to chart

--- deneme/src/chartConfig.js
+++ deneme/src/chartConfig.js
@@ -3,12 +3,12 @@ export const data = {
   datasets: [
     {
       backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-      data: [15, 25]
+      data: [15, 25],
+      hoverOffset: 4,
     }
-  ]
-}
-
+  ],
+};
 export const options = {
   responsive: true,
-  maintainAspectRatio: false,
-}
+  maintainAspectRatio: true,
+};
