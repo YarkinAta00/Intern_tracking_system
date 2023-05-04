8207c6f6201b7a206b488b618caa3df3b419f865
yarkinata <yarkinata6268@gmail.com>
2023-05-04 12:38:14 +0300

    Delete method implemented in to Vue Project

--- deneme/src/Interns.vue
+++ deneme/src/Interns.vue
@@ -1,8 +1,8 @@
 <template>
-    <PageTitle pageTitleValue="Interns"/>
-      <div class="container d-flex align-items-center  justify-content-center my-5">
-          <div id="main-container" class="col d-flex align-items-center  justify-content-center">
-            <table class=" table table-striped table-hover">
+    <PageTitle pageTitleValue="Interns" />
+    <div class="container d-flex align-items-center  justify-content-center my-5">
+        <div id="main-container" class="col d-flex align-items-center  justify-content-center">
+            <table class="table table-striped table-hover table-responsive">
                 <thead>
                     <tr>
                         <th scope="col" class="fs-5">ID</th>
@@ -11,38 +11,42 @@
                         <th scope="col" class="fs-5">Email</th>
                         <th scope="col" class="fs-5">DepartmantID</th>
                         <th scope="col" class="fs-5">RoleID</th>
-                        <th scope="col" class="fs-5">Details</th>
+                        <th scope="col" class="fs-5 px-4">Details</th>
+                        <th scope="col" class="fs-5 px-4">Delete</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider">
                     <tr v-for="dbIntern in dbInterns">
-                        <td class="fs-5" > {{ dbIntern.id }}</td>
-                        <td class="fs-5">{{ dbIntern.firstName }}</td>
-                        <td class="fs-5" >{{ dbIntern.lastName }}</td>
-                        <td class="fs-5" >{{  dbIntern.email}}</td>
-                        <td class="fs-5" >{{  dbIntern.depID}}</td>
-                        <td class="fs-5" >{{  dbIntern.roleID}}</td>
+                        <td class="fs-5"> {{ dbIntern.id }}</td>
+                        <td class="fs-5">{{ dbIntern.firsName }}</td>
+                        <td class="fs-5">{{ dbIntern.lastName }}</td>
+                        <td class="fs-5">{{ dbIntern.email }}</td>
+                        <td class="fs-5">{{ dbIntern.depID }}</td>
+                        <td class="fs-5">{{ dbIntern.roleID }}</td>
                         <td class="fs-5">
-                            <Button @click="goProfile" buttonText="Detail"/>
+                            <router-link :to="`/internprofile/${dbIntern.id}`"
+                                class="btn btn-primary mx-4">Detail</router-link>
                         </td>
-                    </tr>   
-                    <!--
-                    <tr v-for="(intern, index) in interns">
-                        <td class="fs-5" v-text="intern.id"></td>
-                        <td class="fs-5" v-text="intern.name"></td>
-                        <td class="fs-5" v-text="intern.surname"></td>
-                        <td class="fs-5" v-text="intern.department"></td>
-                        <td>
-                            <Button @click="goProfile" buttonText="Detail"/>
+                        <td class="fs-5">
+                            <Button @click=deleteIntern(dbIntern.id) class="btn btn-danger" buttonText="Delete" />
                         </td>
-                    </tr>   
-                    -->
-                   
+                    </tr>
                 </tbody>
             </table>
-          </div>
-      </div>
-  </template>
+        </div>
+    </div>
+    <div class="d-flex align-items-center  justify-content-center">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
 
 <script>
 import Button from './components/Button.vue'
@@ -53,62 +57,38 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            dbInterns:{},
-            interns: [{
-                id: 1,
-                name: "Yarkın",
-                surname: "Ata",
-                department: "Development Team",
-
-            },
-            {
-                id: 2,
-                name: "Merve",
-                surname: "Cengiz",
-                department: "Support Engineer Team",
-
-            },
-            {
-                id: 3,
-                name: "Hakan",
-                surname: "Atik",
-                department: "Project Management Team",
-
-            },
-            {
-                id: 4,
-                name: "Altay",
-                surname: "Taneri",
-                department: "Testng Team",
-
-            },],
+            dbInterns: {},
         }
     },
-    mounted(){
+    mounted() {
         axios.get("https://localhost:7270/api/InternshipTracker/GetUsers")
-        .then((response)=>{
-            this.dbInterns=response.data
-          console.log(response.data)
-        })
+            .then((response) => {
+                this.dbInterns = response.data
+                console.log(response.data)
+            })
     },
     components: {
-    Footer,
-    Button,
-    PageTitle,
-    ProfileNavigateButton
-},
-
-methods: {
-    goProfile() {
-      //debugger
-      this.$router.push('internprofile') 
+        Footer,
+        Button,
+        PageTitle,
+        ProfileNavigateButton
     },
-    goAddPosition(){
-                this.$router.push('addposition') 
-            }
-  },
+    methods: {
+        deleteIntern(id) {
+            axios.delete(`https://localhost:7270/api/InternshipTracker/DeleteUser?id=${id}`)
+                .then((response) => { console.log(response.data) })
+        }
+    },
 }
 </script>
+<style>
+.my-custom-scrollbar {
+position: relative;
+height: 200px;
+overflow: auto;
+}
+.table-wrapper-scroll-y {
+display: block;
+}
+</style>
 
-
-<style></style>
