bdb5e3a34ea1cdb991b67dcbbc610dcd3fbb2ca6
yarkinata <yarkinata6268@gmail.com>
2023-05-14 21:57:14 +0300

    mt-4 deleted from dropdown component

--- deneme/src/components/DropdownMenu.vue
+++ deneme/src/components/DropdownMenu.vue
@@ -9,7 +9,7 @@ export default {
 }
 </script>
 <template>
- <select class="form-select mt-4" aria-label="Default select example">
+ <select class="form-select" aria-label="Default select example">
       <option selected>{{ defaultSelect }}</option>
       <option value="1">Software Engineering</option>
       <option value="2">Computer Engineering</option>
