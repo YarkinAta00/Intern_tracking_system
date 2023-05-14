f7cbe466a6236bd2d5d5be928ca6ce3eeb9fcf72
yarkinata <yarkinata6268@gmail.com>
2023-05-14 21:55:47 +0300

    Interns table modified

--- deneme/src/Interns.vue
+++ deneme/src/Interns.vue
@@ -2,33 +2,33 @@
     <PageTitle pageTitleValue="Interns" />
     <div class="container d-flex align-items-center  justify-content-center my-5">
         <div id="main-container" class="col d-flex align-items-center  justify-content-center">
-            <table class="table table-striped table-hover table-responsive">
+            <table class="table table-striped table-hover table-condensed table-responsive">
                 <thead>
                     <tr>
-                        <th scope="col" class="fs-5">ID</th>
-                        <th scope="col" class="fs-5">Name</th>
-                        <th scope="col" class="fs-5">Surname</th>
-                        <th scope="col" class="fs-5">Email</th>
-                        <th scope="col" class="fs-5">DepartmantID</th>
-                        <th scope="col" class="fs-5">RoleID</th>
-                        <th scope="col" class="fs-5 px-4">Details</th>
-                        <th scope="col" class="fs-5 px-4">Delete</th>
+                        <th scope="col" class="fs-5 px-4">ID</th>
+                        <th scope="col" class="fs-5 px-4">Name</th>
+                        <th scope="col" class="fs-5 px-4">Surname</th>
+                        <th scope="col" class="fs-5 px-5">Email</th>
+                        <th scope="col" class="fs-5 px-4">DepartmantID</th>
+                        <th scope="col" class="fs-5 px-4">RoleID</th>
+                        <th scope="col" class="fs-5 px-5">Details</th>
+                        <th scope="col" class="fs-5 px-5">Delete</th>
                     </tr>
                 </thead>
                 <tbody class="table-group-divider">
                     <tr v-for="dbIntern in dbInterns">
-                        <td class="fs-5"> {{ dbIntern.id }}</td>
-                        <td class="fs-5">{{ dbIntern.firsName }}</td>
-                        <td class="fs-5">{{ dbIntern.lastName }}</td>
-                        <td class="fs-5">{{ dbIntern.email }}</td>
-                        <td class="fs-5">{{ dbIntern.depID }}</td>
-                        <td class="fs-5">{{ dbIntern.roleID }}</td>
-                        <td class="fs-5">
+                        <td class="fs-5 px-4"> {{ dbIntern.id }}</td>
+                        <td class="fs-5 px-4">{{ dbIntern.firstName }}</td>
+                        <td class="fs-5 px-4">{{ dbIntern.lastName }}</td>
+                        <td class="fs-5 px-4">{{ dbIntern.email }}</td>
+                        <td class="fs-5 px-5">{{ dbIntern.depID }}</td>
+                        <td class="fs-5 px-5">{{ dbIntern.roleID }}</td>
+                        <td class="fs-5 px-4">
                             <router-link :to="`/internprofile/${dbIntern.id}`"
-                                class="btn btn-primary mx-4">Detail</router-link>
+                                class="btn btn-primary mx-4"><i class="bi bi-info-lg px-3" style="font-size: 1.3rem;"></i></router-link>
                         </td>
                         <td class="fs-5">
-                            <Button @click=deleteIntern(dbIntern.id) class="btn btn-danger" buttonText="Delete" />
+                            <button onclick="deleteIntern(dbIntern.id)" type="button" class="btn btn-danger px-4 mx-5"><i class="bi bi-trash" style="font-size: 1.3rem;"></i></button>
                         </td>
                     </tr>
                 </tbody>
@@ -51,7 +51,6 @@
 <script>
 import Button from './components/Button.vue'
 import ProfileNavigateButton from './components/ProfileNavigateButton.vue'
-import Footer from './Footer.vue'
 import PageTitle from './components/PageTitle.vue'
 import axios from 'axios'
 export default {
@@ -68,7 +67,6 @@ export default {
             })
     },
     components: {
-        Footer,
         Button,
         PageTitle,
         ProfileNavigateButton
