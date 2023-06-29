0c439d0800609e56801b526ed2bcd5e70d585de0
yarkinata <yarkinata6268@gmail.com>
2023-06-29 18:19:59 +0300

    On-going task can be shown on Home page

--- deneme/src/HomePage.vue
+++ deneme/src/HomePage.vue
@@ -19,6 +19,8 @@ export default {
   data() {
     return {
       userInfo: {},
+      dbTask: {},
+      baseUrl: "https://localhost:7270/api/",
       authAxios: axios.create({
         baseURL: `https://localhost:7270/api/`,
         headers: {
@@ -35,6 +37,7 @@ export default {
       return chartConfig.options
     },
   },
+
   created() {
     this.authAxios.get("InternshipTracker/GetAuthorizedUser")
       .then((response) => {
@@ -45,26 +48,47 @@ export default {
       }).catch(error => { console.log(error) });
   },
 
+  mounted() {
+    axios.get(this.baseUrl + "Task/GetTaskProgress")
+      .then((response) => {
+        this.dbTask = response.data;
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 };
 
 </script>
 <template>
   <div>
     <div class="row mt-4">
-      <PageTitle pageTitleValue="Welcome" :pageUserName= userInfo.userName  />
+      <PageTitle pageTitleValue="Welcome" :pageUserName=userInfo.userName />
     </div>
 
     <div class="hstack px-5 mt-5 gap-5">
       <div class="gap-5">
-
       </div>
       <div>
-
         <Doughnut :width="400" :height="400" :data="data" :options="options" id="internChart" />
       </div>
 
       <div class="vr"></div>
 
+      <div>
+          <div class="container">
+            <h2 class="text-primary mb-4">On Going Task:</h2>
+            <div class="card">
+              <h5 class="card-header">{{ dbTask.taskName }}</h5>
+              <div class="card-body">
+                <h6 class="card-text">- {{ dbTask.taskDetails }}.</h6>
+                <p class="card-text">Sender: {{ dbTask.taskSender }}</p>
+                <a href="#" class="btn btn-success mt-2">Complete</a>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
